9786f26128a1586bec1a1bc5c560c32c6e3aefcc
Pedro Neto <pedroneto12092000@hotmail.com>
2020-10-08 17:30:07 -0300

    Added POST request to API

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <string.h>
 
 #define LOCATION_MOTO (0)
 #define LOCATION_BAT (1)
@@ -8,17 +9,18 @@
 #define INFORMATION_BAT (3)
 
 
-const char* ssid = "kikulindo"; // trocar aqui
-const char* password =  "kikumaravilhoso"; //trocar aqui
+const char* ssid = ""; // trocar aqui
+const char* password =  ""; //trocar aqui
 
-const char* loc_moto = "https://ecov.heroku.com/api/v1/geolocm";
-const char* loc_bat = "https://ecov.heroku.com/api/v1/geolocb";
-const char* info_moto = "https://ecov.heroku.com/api/v1/paramm/";
-const char* info_bat = "https://ecov.heroku.com/api/v1//api/v1/paramb";
+const char* loc_moto = "https://ecov.herokuapp.com/api/v1/geolocm";
+const char* loc_bat = "https://ecov.herokuapp.com/api/v1/geolocb";
+const char* info_moto = "https://ecov.herokuapp.com/api/v1/paramm/";
+const char* info_bat = "https://ecov.herokuapp.com/api/v1/paramb";
 
 static uint8_t cont = 0;
 static float lat[12] = {-23.5565750, 23.5558840 , -23.5565680, -23.5572540, -23.5579190, -23.5587600, -23.5597660, -23.5606090, -23.5668970, -23.5735450, -23.5814590, -23.5860590};
 static float lon[12] = {-46.6641230, -46.6633810, -46.6620300, -46.6609920, -46.6602770, -46.6592620, -46.6581830, -46.6572770, -46.6500340, -46.6531680, -46.6623880, -46.6605720};
+
 
 void request(const char* URL, int type_i){
 
@@ -30,9 +32,25 @@ void request(const char* URL, int type_i){
 
     if (type_i == LOCATION_MOTO || type_i == LOCATION_BAT){
       
-      http.addHeader("Content-Type", "text/plain");             //Specify content-type header //provavelmente é outro parâmetro aqui
+      http.addHeader("Content-Type", "application/json");             //Specify content-type header //provavelmente é outro parâmetro aqui
 
-      int httpResponseCode = http.POST("MSG aqui");   //Send the actual POST request //colocar valores de lat e lon aqui, já tem um contador definido (cont) pode usar ele pra incrementar a posiçao da lista
+      char uuid[69] = "{\"uuid\":";
+      char uuid_cont[20] = "\"uuid-aparece-aqui\"";
+      strcat(uuid, uuid_cont);
+
+      char latstr[20] = ", \"lat\":";
+      char lat_cont [12];
+      sprintf(lat_cont, "%f", lat[cont]);
+      strcat(latstr, lat_cont);
+      strcat(uuid, latstr);
+
+      char lonstr[20] = ", \"lon\":";
+      char lon_cont [12];
+      sprintf(lon_cont, "%f}", lon[cont]);
+      strcat(lonstr, lon_cont);
+      strcat(uuid, lonstr);
+
+      int httpResponseCode = http.POST(uuid);   //Send the actual POST request //colocar valores de lat e lon aqui, já tem um contador definido (cont) pode usar ele pra incrementar a posiçao da lista
 
       if(httpResponseCode>0){
 
@@ -105,5 +123,5 @@ void loop() {
   }
   if (cont < 12) cont++;
   else cont = 0;
-  delay(800);  //Send a request every 0.8 seconds
+  delay(1000);  //Send a request every 0.8 seconds
 }
